bd3fd4124519c6fc07246ee884eb6a9a99472724
Vedat Zeybek <vedatzeybek20@gmail.com>
2025-09-10 21:21:02 +0300

    cpp07

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,26 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include "iostream"
+
+template <typename T>
+void	swap(T &a, T &b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T	min(const T &a, const T &b)
+{
+	return (a > b ? b : a);
+}
+
+template <typename T>
+T	max(const T &a, const T &b)
+{
+	return (a < b ? b : a);
+}
+
+#endif
