10ec37870beb9624cea7c936ef238523b45904be
Vedat Zeybek <vedatzeybek20@gmail.com>
2025-09-11 13:16:00 +0300

    cpp08

---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,5 @@
+//Container	Random Access	Ortadan Ekle/Sil	Sona Ekle/Sil	Hafıza Yapısı
+//vector	✅ hızlı	❌ yavaş	✅ hızlı	Ardışık
+//list	❌ yok	✅ hızlı	✅ hızlı	Linked list
+//deque	✅ hızlı	❌ orta	✅ hızlı	Blok blok
+
